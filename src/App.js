27a2d4d50ae23e6ba80d7a6cf88b9e57d76b499c
Alex Holyk <sasha.holyk@gmail.com>
2021-03-04 13:27:26 +0200

    fetch products in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const URL =
+  'https://pocket-rocket-public.s3.eu-central-1.amazonaws.com/code-challenge/products.json';
+
+const App = () => {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const result = await axios.get(URL);
+      setProducts(result.data?.resources);
+    };
+
+    fetchProducts();
+  }, []);
+
   return <div className='App'></div>;
-}
+};
 
 export default App;
