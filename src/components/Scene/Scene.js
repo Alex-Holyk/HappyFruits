55ed10ab570fd8cfda2fd0a968a56e9784b10f4d
Alex Holyk <sasha.holyk@gmail.com>
2021-03-04 18:35:31 +0200

    add Baskets to the Scene

--- src/components/Scene/Scene.js
+++ src/components/Scene/Scene.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Scene = () => {
-  return <div>Scene</div>;
+import './Scene.css';
+import Basket from './Basket/Basket';
+
+const Scene = ({ basketProducts, products }) => {
+  debugger;
+  const basketImgUrl = products[basketProducts.basketId].image_url;
+
+  return (
+    <div className='scene-container'>
+      <Basket imgUrl={basketImgUrl} />
+    </div>
+  );
 };
 
 export default Scene;
