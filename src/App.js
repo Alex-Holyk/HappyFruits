de812a4742c6017b3d2ebff38cf5fa7f193b1318
Alex Holyk <sasha.holyk@gmail.com>
2021-03-04 14:56:27 +0200

    add Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import BasketBuilder from './containers/BasketBuilder/BasketBuilder';
 import './App.css';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className='app'>
+      <Header />
       <BasketBuilder />
     </div>
   );
